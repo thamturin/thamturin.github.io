2ea5f9307957cb6faef53a762aa6bac2d1d63226
thamturin <ahihidiscord@gmail.com>
2022-08-12 20:04:16 +0700

    Release thamturin.github.io

---
+++ README.md
@@ -0,0 +1,2 @@
+# coded by rf0x1d
+# fork and re-write by rin
---
+++ assets/js/text.js
@@ -0,0 +1,28 @@
+$(function() {
+    var title = document.title,
+      animSeq = ["|"],
+        animIndex = 0,
+        titleIndex = 0;
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0
+        }
+        if (animIndex > 1) {
+            titleIndex++;
+            animIndex = 0
+        }
+        document.title = loadTitle + animSeq[0];
+        animIndex++
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+$(function() {
+  $(".typed").typed({
+      strings: ["Yo, my name is Rin", "I love you", "Im cool"],
+      typeSpeed: 150,
+      loop: true,
+      cursorChar: "|",
+  });
+});
